c6f4772f62d9232f66eb20f6e7d212fc7f737739
telontelvarost <telontelvarost@gmail.com>
2024-01-08 14:32:12 -0500

    ISSUE-8 - Fix up death screen mixin

--- src/main/java/com/github/telvarost/finalbeta/mixin/DeathScreenMixin.java
+++ src/main/java/com/github/telvarost/finalbeta/mixin/DeathScreenMixin.java
@@ -3,10 +3,10 @@ package com.github.telvarost.finalbeta.mixin;
 import com.github.telvarost.finalbeta.Config;
 import net.minecraft.client.gui.screen.ScreenBase;
 import net.minecraft.client.render.TextRenderer;
+import net.minecraft.entity.player.PlayerBase;
+import net.modificationstation.stationapi.api.entity.player.PlayerHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import net.minecraft.client.gui.screen.ingame.Death;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(Death.class)
 public class DeathScreenMixin extends ScreenBase {
 
-    public DeathScreenMixin() {
+	public DeathScreenMixin() {
     }
 
 	@Redirect(
@@ -26,6 +26,14 @@ public class DeathScreenMixin extends ScreenBase {
 			)
 	)
 	private void finalBeta_getResourcesUrl(Death instance, TextRenderer textRenderer, String s, int i, int j, int k) {
-		this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7e" + "0", i, j, k);
+		int currentScore = 0;
+		PlayerBase player = PlayerHelper.getPlayerFromGame();
+
+		if (null != player)
+		{
+			currentScore = player.score;
+		}
+
+		this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7e" + currentScore, i, j, k);
 	}
 }

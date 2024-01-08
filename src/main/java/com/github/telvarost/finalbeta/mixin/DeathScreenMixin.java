77a7bade169b93d26eb40daffc3be1ee5145eb26
telontelvarost <telontelvarost@gmail.com>
2024-01-08 14:20:40 -0500

    ISSUE-8 - Remove mixins that were breaking the mod; Fix death screen mixin sorta

--- src/main/java/com/github/telvarost/finalbeta/mixin/DeathScreenMixin.java
+++ src/main/java/com/github/telvarost/finalbeta/mixin/DeathScreenMixin.java
@@ -1,17 +1,31 @@
 package com.github.telvarost.finalbeta.mixin;
 
 import com.github.telvarost.finalbeta.Config;
+import net.minecraft.client.gui.screen.ScreenBase;
+import net.minecraft.client.render.TextRenderer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import net.minecraft.client.gui.screen.ingame.Death;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Death.class)
-public class DeathScreenMixin {
+public class DeathScreenMixin extends ScreenBase {
 
-	@ModifyConstant(method = "render", constant = @Constant(stringValue = "Score: &e"))
-	private String finalBeta_getResourcesUrl(String def) {
-		return def.replace('&', '\u00a7'); // Replacing & with § so the color actually displays as yellow
+    public DeathScreenMixin() {
+    }
+
+	@Redirect(
+			method = "render",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/client/gui/screen/ingame/Death;drawTextWithShadowCentred(Lnet/minecraft/client/render/TextRenderer;Ljava/lang/String;III)V",
+					ordinal = 1
+			)
+	)
+	private void finalBeta_getResourcesUrl(Death instance, TextRenderer textRenderer, String s, int i, int j, int k) {
+		this.drawTextWithShadowCentred(textRenderer, "Score: \u00a7e" + "0", i, j, k);
 	}
 }
--- src/main/java/com/github/telvarost/finalbeta/mixin/OverlayMixin.java
+++ src/main/java/com/github/telvarost/finalbeta/mixin/OverlayMixin.java
@@ -21,26 +21,26 @@ public class OverlayMixin extends DrawableHelper {
 	@Shadow
 	private Minecraft minecraft;
 	
-	@Inject(
-		method = "renderHud",
-		at = @At(
-			value = "INVOKE", 
-			target = "Lorg/lwjgl/opengl/GL11;glPopMatrix()V"
-		),
-		slice = @Slice(
-			from = @At(
-				value = "INVOKE", 
-				target = "Lnet/minecraft/client/render/TextRenderer;drawTextWithShadow(Ljava/lang/String;III)V"
-			), 
-			to = @At(
-				value = "FIELD", 
-				target = "Lnet/minecraft/client/gui/InGame;jukeboxMessageTime:I",
-				opcode = Opcodes.GETFIELD
-			)
-		)
-	)
-	public void finalBeta_render(float f, boolean flag, int i, int j, CallbackInfo ci) {
-		TextRenderer textRenderer = this.minecraft.textRenderer;
-		this.drawTextWithShadow(textRenderer, "Days Played: " + ModHelper.getGameDaysPlayed() + " (" + ModHelper.getRealDaysPlayed() + ")", 2, 96, 14737632);
-	}
+//	@Inject(
+//		method = "renderHud",
+//		at = @At(
+//			value = "INVOKE",
+//			target = "Lorg/lwjgl/opengl/GL11;glPopMatrix()V"
+//		),
+//		slice = @Slice(
+//			from = @At(
+//				value = "INVOKE",
+//				target = "Lnet/minecraft/client/render/TextRenderer;drawTextWithShadow(Ljava/lang/String;III)V"
+//			),
+//			to = @At(
+//				value = "FIELD",
+//				target = "Lnet/minecraft/client/gui/InGame;jukeboxMessageTime:I",
+//				opcode = Opcodes.GETFIELD
+//			)
+//		)
+//	)
+//	public void finalBeta_render(float f, boolean flag, int i, int j, CallbackInfo ci) {
+//		TextRenderer textRenderer = this.minecraft.textRenderer;
+//		this.drawTextWithShadow(textRenderer, "Days Played: " + ModHelper.getGameDaysPlayed() + " (" + ModHelper.getRealDaysPlayed() + ")", 2, 96, 14737632);
+//	}
 }

f50b758175b7f006e3b54b5386080c1d19dbf447
telontelvarost <telontelvarost@gmail.com>
2024-01-08 15:04:30 -0500

    ISSUE-8 - Order config settings alphabetically

--- src/main/java/com/github/telvarost/finalbeta/Config.java
+++ src/main/java/com/github/telvarost/finalbeta/Config.java
@@ -10,17 +10,20 @@ public class Config {
 
     public static class ConfigFields {
 
-        @ConfigName("Allow sugarcane to be placed on sand")
-        public static Boolean sugarCaneFixesEnabled = true;
-
-        @ConfigName("Fix bow models")
-        public static Boolean FIX_BOW_MODEL = true;
+        @ConfigName("Add day counter to F3 overlay")
+        public static Boolean ADD_DAY_COUNTER = true;
 
         @ConfigName("Add more sounds")
         public static Boolean ADD_MORE_SOUNDS = true;
 
-        @ConfigName("Add day counter to F3 overlay")
-        public static Boolean ADD_DAY_COUNTER = true;
+        @ConfigName("Allow sugarcane to be placed on sand")
+        public static Boolean sugarCaneFixesEnabled = true;
+
+        @ConfigName("Disable id tags")
+        public static Boolean DISABLE_ID_TAGS = true;
+
+        @ConfigName("Fix bow models")
+        public static Boolean FIX_BOW_MODEL = true;
 
         @ConfigName("Fix fishing")
         public static Boolean FIX_FISHING = true;
@@ -28,17 +31,14 @@ public class Config {
         @ConfigName("Fix furnace lava bucket")
         public static Boolean FIX_FURNACE_LAVA_BUCKET = true;
 
-        @ConfigName("Disable id tags")
-        public static Boolean DISABLE_ID_TAGS = true;
-
-        @ConfigName("Fix minecart stopping on items")
-        public static Boolean FIX_MINECART_STOPPING_ON_ITEMS = true;
+        @ConfigName("Fix leg armor on vehicles")
+        public static Boolean FIX_LEG_ARMOR_ON_VEHICLES = true;
 
         @ConfigName("Fix minecart flickering")
         public static Boolean FIX_MINECART_FLICKERING = true;
 
-        @ConfigName("Fix leg armor on vehicles")
-        public static Boolean FIX_LEG_ARMOR_ON_VEHICLES = true;
+        @ConfigName("Fix minecart stopping on items")
+        public static Boolean FIX_MINECART_STOPPING_ON_ITEMS = true;
 
     }
 }

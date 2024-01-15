1eb9e172f87771dd9e0c8d3656bf77718ee754a1
telontelvarost <telontelvarost@gmail.com>
2024-01-14 19:13:18 -0500

    ISSUE-15 - Fix pressure plates not updating when adjacent block changes

--- src/main/java/com/github/telvarost/finalbeta/mixin/PressurePlateMixin.java
+++ src/main/java/com/github/telvarost/finalbeta/mixin/PressurePlateMixin.java
@@ -5,17 +5,11 @@ import net.minecraft.block.BlockBase;
 import net.minecraft.block.PressurePlate;
 import net.minecraft.block.PressurePlateTrigger;
 import net.minecraft.block.material.Material;
-import net.minecraft.level.BlockView;
 import net.minecraft.level.Level;
-import net.minecraft.util.maths.Box;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PressurePlate.class)
 class PressurePlateMixin extends BlockBase {
@@ -47,10 +41,29 @@ class PressurePlateMixin extends BlockBase {
         }
     }
 
-    @Inject(at = @At("HEAD"), method = "onAdjacentBlockUpdate", cancellable = true)
-    public void annoyanceFix_onAdjacentBlockUpdate(Level arg, int i, int j, int k, int l, CallbackInfo ci) {
+    @Redirect(
+            method = "onAdjacentBlockUpdate",
+            at = @At(
+                value = "INVOKE",
+                target = "Lnet/minecraft/level/Level;canSuffocate(III)Z"
+            )
+    )
+    public boolean annoyanceFix_onAdjacentBlockUpdate(Level instance, int i, int j, int k) {
         if (Config.ConfigFields.pressurePlateFixesEnabled) {
-            ci.cancel();
+            int blockBelowPressurePlateId = instance.getTileId(i, j, k);
+
+            if (BlockBase.FENCE.id == blockBelowPressurePlateId)
+            {
+                return true;
+            }
+            else
+            {
+                return instance.canSuffocate(i, j, k);
+            }
+        }
+        else
+        {
+            return instance.canSuffocate(i, j, k);
         }
     }
 }

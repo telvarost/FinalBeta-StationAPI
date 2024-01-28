f06f308c0b97541cfae9f1e4ae145ba45b07e0b9
telontelvarost <telontelvarost@gmail.com>
2024-01-28 14:29:33 -0500

    UDOC-0 - Fix sliders not working in video settings menu

--- src/main/java/com/github/telvarost/finalbeta/mixin/options/VideoSettingsMixin.java
+++ src/main/java/com/github/telvarost/finalbeta/mixin/options/VideoSettingsMixin.java
@@ -1,10 +1,17 @@
 package com.github.telvarost.finalbeta.mixin.options;
 
 import com.github.telvarost.finalbeta.ModOptions;
+import net.minecraft.client.gui.screen.ScreenBase;
 import net.minecraft.client.gui.screen.menu.VideoSettings;
+import net.minecraft.client.gui.widgets.Button;
+import net.minecraft.client.gui.widgets.OptionButton;
+import net.minecraft.client.options.GameOptions;
 import net.minecraft.client.options.Option;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Arrays;
 
@@ -12,14 +19,34 @@ import java.util.Arrays;
  *  See: https://github.com/DanyGames2014/UniTweaks
  */
 @Mixin(VideoSettings.class)
-public class VideoSettingsMixin {
+public class VideoSettingsMixin extends ScreenBase {
 	@Shadow
 	private static Option[] OPTIONS;
+
+	@Shadow private GameOptions options;
+
+	@Shadow private ScreenBase parent;
 
 	static {
 		OPTIONS = Arrays.copyOf(OPTIONS, OPTIONS.length + 3);
 		VideoSettingsMixin.OPTIONS[VideoSettingsMixin.OPTIONS.length - 3] = ModOptions.fogDensityOption;
 		VideoSettingsMixin.OPTIONS[VideoSettingsMixin.OPTIONS.length - 2] = ModOptions.cloudsOption;
 		VideoSettingsMixin.OPTIONS[VideoSettingsMixin.OPTIONS.length - 1] = ModOptions.cloudHeightOption;
+	}
+
+	@Inject(method = "buttonClicked", at = @At("HEAD"), cancellable = true)
+	protected void buttonClicked(Button arg, CallbackInfo ci) {
+		if (arg.active) {
+			if (arg.id < 100 && arg instanceof OptionButton) {
+				this.options.changeOption(((OptionButton) arg).getOption(), 1);
+				arg.text = this.options.getTranslatedValue(Option.getById(arg.id));
+			}
+
+			if (arg.id == 200) {
+				this.minecraft.options.saveOptions();
+				this.minecraft.openScreen(this.parent);
+			}
+		}
+		ci.cancel();
 	}
 }
